e05f64fd759968e6333a430cb7fbf35b6ad4d54e
Zhdanov Bogdan <z-bogdan@outlook.com>
2020-11-22 20:54:25 +0700

    Added random colors and connected ui to py

--- main.py
+++ main.py
@@ -3,6 +3,35 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtGui import QPainter, QColor
 import random
 import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(380, 300)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.DrawButton = QtWidgets.QPushButton(self.centralwidget)
+        self.DrawButton.setGeometry(QtCore.QRect(140, 220, 100, 25))
+        self.DrawButton.setMinimumSize(QtCore.QSize(100, 0))
+        self.DrawButton.setObjectName("DrawButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 380, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.DrawButton.setText(_translate("MainWindow", "Draw"))
 
 
 class Window(QMainWindow):
@@ -25,7 +54,10 @@ class Window(QMainWindow):
         self.repaint()
 
     def drawcircle(self, qp):
-        qp.setBrush(QColor('yellow'))
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        qp.setBrush(QColor(r, g, b))
         r = random.randint(15, 150)
         x = random.randint(15, 380 - r // 2)
         y = random.randint(15, 300 - r // 2)
